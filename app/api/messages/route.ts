8996a5bba7bc5bdab27e7647318fd12be6a4068d
EdwardMelendezM <edward.melendez.mendigure@gmail.com>
2024-02-11 21:37:31 -0500

    refactor endpoint to put and delete message

--- app/api/messages/route.ts
+++ app/api/messages/route.ts
@@ -1,5 +1,6 @@
-import {getCurrentUser} from "@/lib/get-current-user";
 import {NextResponse} from "next/server";
+
+import {getCurrentUser} from "@/lib/get-current-user";
 import {db} from "@/lib/db";
 
 export async function POST(
@@ -70,52 +71,6 @@ export async function POST(
         return NextResponse.json(newMessage)
     } catch (error) {
         console.log(error, "[ERROR_MESSAGE]")
-        return new NextResponse("Internal Server Error", {status: 500})
-    }
-
-}
-
-
-export async function PUT(
-    request: Request
-) {
-    try {
-        const currentUser = await getCurrentUser()
-        const body = await request.json()
-
-        const {
-            messageId,
-            conversationId,
-            message
-        } = body
-
-        if (!currentUser?.id || !currentUser?.phone) {
-            return new NextResponse("Unauthorized", {status: 401})
-        }
-
-        const updateMessage = await db.message.update({
-            where: {
-                id: messageId
-            },
-            data: {
-                body: {
-                    message
-                },
-                conversation: {
-                    connect: {
-                        id: conversationId
-                    }
-                },
-            },
-            include: {
-                seen: true,
-                sender: true,
-            }
-        })
-
-        return NextResponse.json(updateMessage)
-    } catch (error) {
-        console.log(error, "[ERROR_UPDATE_MESSAGE]")
         return new NextResponse("Internal Server Error", {status: 500})
     }
 
